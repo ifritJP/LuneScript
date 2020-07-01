135e39261553d6ad5987659fb867d51855834e57
ifritJP <15117169+ifritJP@users.noreply.github.com>
2020-07-01 22:09:08 +0900

    change version.

--- src/legacy/lua51/lune/base/Option.lua
+++ src/legacy/lua51/lune/base/Option.lua
@@ -200,7 +200,7 @@ local Ast = _lune.loadModule( 'lune.base.Ast' )
 
 local function getBuildCount(  )
 
-   return 2919
+   return 2920
 end
 
 
--- src/legacy/lua51/lune/base/Ver.lua
+++ src/legacy/lua51/lune/base/Ver.lua
@@ -9,7 +9,7 @@ if not _lune2 then
    _lune2 = _lune
 end
 
-local version = "1.1.7"
+local version = "1.1.8"
 _moduleObj.version = version
 
 
