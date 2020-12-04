b7e128ad685f89d6ef9384c965836d7007b64220
ifritJP <15117169+ifritJP@users.noreply.github.com>
2020-12-04 21:20:23 +0900

    add binding.go.

--- src/lune/base/Option.go
+++ src/lune/base/Option.go
@@ -82,7 +82,7 @@ func Option_convExp517(arg1 []LnsAny) LnsAny {
 }
 // 48: decl @lune.@base.@Option.getBuildCount
 func Option_getBuildCount_1015_() LnsInt {
-    return 6268
+    return 6270
 }
 
 // 73: decl @lune.@base.@Option.getRuntimeModule
--- src/lune/base/Option.lua
+++ src/lune/base/Option.lua
@@ -283,7 +283,7 @@ local Ast = _lune.loadModule( 'lune.base.Ast' )
 
 local function getBuildCount(  )
 
-   return 6268
+   return 6270
 end
 
 
