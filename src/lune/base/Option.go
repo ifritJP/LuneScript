d7401780a555605b0178e73d33245406295a9590
ifritJP <15117169+ifritJP@users.noreply.github.com>
2021-06-26 21:42:55 +0900

    add runtime5.lua.

--- src/lune/base/Option.go
+++ src/lune/base/Option.go
@@ -121,7 +121,7 @@ func Option_convExp0_711(arg1 []LnsAny) LnsAny {
 }
 // 52: decl @lune.@base.@Option.getBuildCount
 func Option_getBuildCount_1_(_env *LnsEnv) LnsInt {
-    return 11522
+    return 11523
 }
 
 // 81: decl @lune.@base.@Option.getRuntimeModule
--- src/lune/base/runtime_go/legacy/lua51/lune/base/Option.lua
+++ src/lune/base/runtime_go/legacy/lua51/lune/base/Option.lua
@@ -178,7 +178,7 @@ local Ast = _lune.loadModule( 'lune.base.Ast' )
 
 local function getBuildCount(  )
 
-   return 11522
+   return 11524
 end
 
 
