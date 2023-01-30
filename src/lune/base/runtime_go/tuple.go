c9f1b77ff22443eea2dd17811eae00d06416c17d
ifritJP <15117169+ifritJP@users.noreply.github.com>
2023-01-30 21:50:24 +0900

    fix: change the golang's version to v.18.

---
+++ src/lune/base/runtime_go/tuple.go
@@ -0,0 +1,109 @@
+/*
+MIT License
+
+Copyright (c) 2018,2020 ifritJP
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package runtimelns
+type LnsTuple1 [T1 any] struct {
+    Val1 T1
+}
+
+type LnsTuple2 [T1, T2 any] struct {
+    Val1 T1
+    Val2 T2
+}
+
+type LnsTuple3 [T1, T2, T3 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+}
+
+type LnsTuple4 [T1, T2, T3, T4 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+}
+
+type LnsTuple5 [T1, T2, T3, T4, T5 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+}
+
+type LnsTuple6 [T1, T2, T3, T4, T5, T6 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+    Val6 T6
+}
+
+type LnsTuple7 [T1, T2, T3, T4, T5, T6, T7 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+    Val6 T6
+    Val7 T7
+}
+
+type LnsTuple8 [T1, T2, T3, T4, T5, T6, T7, T8 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+    Val6 T6
+    Val7 T7
+    Val8 T8
+}
+
+type LnsTuple9 [T1, T2, T3, T4, T5, T6, T7, T8, T9 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+    Val6 T6
+    Val7 T7
+    Val8 T8
+    Val9 T9
+}
+
+type LnsTuple10 [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10 any] struct {
+    Val1 T1
+    Val2 T2
+    Val3 T3
+    Val4 T4
+    Val5 T5
+    Val6 T6
+    Val7 T7
+    Val8 T8
+    Val9 T9
+    Val10 T10
+}
